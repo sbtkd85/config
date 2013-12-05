21423239c04f69d9583fa9be76ee6d6050756072
sbtkd85 <sbtkd85@gmail.com>
2013-12-05 10:29:03 -0600

    Fixed read options for zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,13 +2,17 @@
 function proxy()
 {
     echo -n "username:"
-    read -e username
+    read username
     echo -n "password:"
-    read -es password
+    read -s password
     export http_proxy="http://$username:$password@proxyserver:port/"
+    export HTTP_PROXY=$http_proxy
     export https_proxy=$http_proxy
+    export HTTPS_PROXY=$http_proxy
     export ftp_proxy=$http_proxy
+    export FTP_PROXY=$http_proxy
     export rsync_proxy=$http_proxy
+    export RSYNC_PROXY=$http_proxy
     export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
     echo -e "\nProxy environment variable set."
 }
