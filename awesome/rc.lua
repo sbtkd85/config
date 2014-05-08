79a8647c078e472fd12673e01317c20bc77e87eb
sbtkd85 <sbtkd85@gmail.com>
2014-05-08 11:43:17 -0500

    Update rc.lua
    Dynamic .config/ support.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/home/bakshs/.config/awesome/themes/awesome-solarized/dark/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/awesome-solarized/dark/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "terminator"
